3ffd3455180c573986d2a115e3d8e079109584d8
zawlwin223 <zawlwinp223@gmail.com>
2025-07-12 21:47:41 -0700

    Added:adding discount items

---
+++ app/(root)/admin/(main)/discount-items/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold text-gray-800">Discount items Page</h1>
+      <p className="mt-4 text-gray-600">This is discount items page</p>
+    </div>
+  )
+}
